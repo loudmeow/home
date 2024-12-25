f6ac18d7c16282645d25234fa7f67c7fe1039d76
loudmeow <sovlad2006@gmail.com>
2024-12-25 20:20:45 +0300

    end

--- homework 3.py
+++ homework 3.py
@@ -11,3 +11,10 @@ if  a.isdigit() :
         print(f'{sum}')
 else:
     print('Неправильно введено число')
+
+
+
+
+for i in range(1, 10):
+    for j in range(1, 10):
+        print(i, "*", j, "=", i * j, end='\t')
