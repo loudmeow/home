e380f6844fc4cd55a3377f7c69fd8b64f9dfca02
loudmeow <sovlad2006@gmail.com>
2024-12-25 20:27:05 +0300

    end

--- homework 4.py
+++ homework 4.py
@@ -1,8 +1,16 @@
-a = input('Введите текст:')
-с = a.replace('н', '!')
-b = a.count('нн')
-print(с)
-print(b)
+string = str(input())
+n_count = 0
+n_count_max = 0
+for i in string:
+    if i == 'н':
+        n_count += 1
+    else:
+        if n_count_max < n_count:
+            n_count_max = n_count
+        n_count = 0
+stirng = string.replace('н', '!')
+print(n_count_max)
+print(stirng)
 
 
 
@@ -15,3 +23,10 @@ while a[i] != ')':
     print(a[i], end='')
     i += 1
 
+
+
+
+text = str(input())
+for i in text.split():
+    if(i.startswith("а") or i.endswith("я")):
+        print(i)
