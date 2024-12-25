653c7898e38ebdf0d9419a9d190b21be9ca31aee
loudmeow <sovlad2006@gmail.com>
2024-12-25 20:51:45 +0300

    end

--- homework 7.py
+++ homework 7.py
@@ -1,18 +1,30 @@
 
-def read_last(lines, f):
-    if lines < 0 and not lines.isdigig():
-     print('Задано не положительное натуральное число')
-     return
-    b = 0
-    for i in range(0,lines+1):
-        b = b+1
-        last_lines = n - b
-    for row in rows[last_lines:]:
-        print (rows)
-f = open('text1.txt', encoding = 'utf-8' )   
-lines = input('Введите количество последних выводимых строк:')
-n = len(f.readlines())
+def read_last(lines, file):
+    if not isinstance(lines, int) or lines < 1:
+        print("Введено неправльное значение.")
+        return
+    try:
+        with open(file, 'r', encoding='utf-8') as f:
+            file_lines = f.readlines()
+            start_index = max(0, len(file_lines) - lines)
+            for line in file_lines[start_index:]:
+                print(line.strip())
+    except FileNotFoundError:
+        print("Такого файла нет.")
+read_last(2, 'text1')\
+lines = input()
 
 
 
 
+
+filename = input("Введите имя файла: ")
+if not filename.endswith(".txt"):
+    filename = filename + ".txt"
+with open(filename, "w") as f:
+    while True:
+        line = input("Введите строку для записи в файл:n")
+        if line == "":
+            print("Файл сохранен успешно.")
+            break
+        f.write(line + "n")
